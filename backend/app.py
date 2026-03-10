e3d1517edbc0f0471ae1641468e0b7f2c3f5b2cd
icarl35 <icarl35@gmail.com>
2026-03-10 15:08:55 +0100

    initial commit

--- backend/app.py
+++ backend/app.py
@@ -261,3 +261,19 @@ def clear_logs():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
+
+from flask import Flask
+from prometheus_client import make_wsgi_app
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+
+app = Flask(__name__)
+
+# Ton code Flask normal ici
+@app.route('/')
+def home():
+    return "Hello world"
+
+# Ajouter endpoint /metrics
+app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
+    '/metrics': make_wsgi_app()
+})
